3050917a196689c10d5afa2215704d9a7c4f4168
funksoup <josephine@funksoup.com>
2020-06-17 16:29:52 -0400

    Updates

---
+++ precache-manifest.aae2b01a81fd5ec6de0ccb7040bee14c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ffcea9b121462645aca8384c03a5d3",
+    "url": "/serve-me-dad-jokes/index.html"
+  },
+  {
+    "revision": "56133a9d7a5556d3a17b",
+    "url": "/serve-me-dad-jokes/static/css/main.7e4e8778.chunk.css"
+  },
+  {
+    "revision": "ef18c46c3d075fdbd44f",
+    "url": "/serve-me-dad-jokes/static/js/2.cfceb7ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/serve-me-dad-jokes/static/js/2.cfceb7ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56133a9d7a5556d3a17b",
+    "url": "/serve-me-dad-jokes/static/js/main.7f72d8bb.chunk.js"
+  },
+  {
+    "revision": "14fee24f37e90d0762ff",
+    "url": "/serve-me-dad-jokes/static/js/runtime-main.2ba12875.js"
+  }
+]);
